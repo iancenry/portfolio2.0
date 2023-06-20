6f6d04bf30829dc38c53cd30f529da90ded2d5d8
Ian Cenry <iancenry@gmail.com>
2023-06-20 22:53:01 +0300

    update & style navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,46 @@
+import { Link } from 'react-router-dom'
+import './Navbar.scss'
+
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <header>
+      <nav className="navbar flex flex-jc-sb flex-ai-c">
+        <div className="navbar__logo">
+          <img src="/src/assets/images/logo.png" alt="portfolio logo" />
+        </div>
+        <ul className="navbar__links flex flex-jc-c flex-ai-c">
+          <li>
+            <Link to="." className="p__firacode">
+              <span>#</span>home
+            </Link>
+          </li>
+          <li className="p__firacode">
+            <Link to="projects" className="p__firacode">
+              <span>#</span>projects
+            </Link>
+          </li>
+          <li className="p__firacode">
+            <Link to="about" className="p__firacode">
+              <span>#</span>about
+            </Link>
+          </li>
+          <li className="p__firacode">
+            <Link to="contact" className="p__firacode">
+              <span>#</span>contact
+            </Link>
+          </li>
+        </ul>
+        <div className="navbar__login flex flex-jc-c flex-ai-c">
+          <Link to="." className="p__firacode">
+            Login
+          </Link>
+        </div>
+        {/* smallscreen navbar */}
+      </nav>
+    </header>
+  )
 }
 
 export default Navbar
+
+//TODO: add smallscreen navbar and put login to side as icon not words
