f0a703c42ecfd02e8d8919ab86201abc7eebc45b
Ian Cenry <iancenry@gmail.com>
2023-06-20 20:31:38 +0300

    initial pages

---
+++ src/Pages/index.js
@@ -0,0 +1,7 @@
+import Home from './Home/Home'
+import Projects from './Projects/Projects'
+import About from './About/About'
+import Contact from './Contact/Contact'
+import NotFound from './NotFound/NotFound'
+
+export { Home, Projects, About, Contact, NotFound }
