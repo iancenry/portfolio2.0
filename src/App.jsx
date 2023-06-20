e6d459dc9237a5485c7706217c026beabc752c00
Ian Cenry <iancenry@gmail.com>
2023-06-20 21:49:18 +0300

    update basic styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import './sass/App.scss'
+import { Routes, Route } from 'react-router-dom'
+import { Home, Projects, About, Contact, NotFound } from './Pages'
+import { Layout } from './Components'
 
 function App() {
-  return <></>
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<Home />} />◘
+        <Route path="/projects" element={<Projects />} />◘
+        <Route path="/about" element={<About />} />◘
+        <Route path="/contact" element={<Contact />} />◘
+        <Route path="*" element={<NotFound />} />◘
+      </Route>
+    </Routes>
+  )
 }
 
 export default App
