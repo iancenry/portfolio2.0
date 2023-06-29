d321d90af9cbe094e846ebdac3ad33cdbd57f659
Ian Cenry <iancenry@gmail.com>
2023-06-29 22:42:43 +0300

    add prop validation: ProjectCard.jsx

--- src/Components/ProjectCard/ProjectCard.jsx
+++ src/Components/ProjectCard/ProjectCard.jsx
@@ -1,9 +1,18 @@
 import './ProjectCard.scss'
+import PropTypes from 'prop-types'
 
 const ProjectCard = ({ cover, title, description, languages }) => {
-  return <div>ProjectCard</div>
+  return (
+   
+  )
+}
+
+ProjectCard.propTypes = {
+  cover: PropTypes.string,
+  languages: PropTypes.array,
+  title: PropTypes.string, 
+  description: PropTypes.string
+  
 }
 
 export default ProjectCard
-
-//  export props to data.js
