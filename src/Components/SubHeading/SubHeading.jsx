1cf8aa9b960c1f68f63bf372b39e0fba626d141d
Ian Cenry <iancenry@gmail.com>
2023-06-30 19:39:23 +0300

    update Subheading styles

--- src/Components/SubHeading/SubHeading.jsx
+++ src/Components/SubHeading/SubHeading.jsx
@@ -2,12 +2,10 @@ import './SubHeading.scss'
 
 const SubHeading = ({ subheading }) => {
   return (
-    <>
-      <p className="h3__firacode subtitle" style={{ fontSize: '2rem' }}>
-        <span>#</span>
-        {subheading}
-      </p>
-    </>
+    <p className="h3__firacode subtitle" style={{ fontSize: '2rem' }}>
+      <span>#</span>
+      {subheading}
+    </p>
   )
 }
 
