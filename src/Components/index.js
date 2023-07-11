55647b4fa44d8f2bfa169920e188ce653db2f13d
Ian Cenry <iancenry@gmail.com>
2023-07-11 23:16:14 +0300

    export component

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,7 +1,8 @@
 import Navbar from './Navbar/Navbar'
 import Footer from './Footer/Footer'
 import ProjectCard from './ProjectCard/ProjectCard'
+import SkillsCard from './SkillsCard/SkillsCard'
 import SubHeading from './SubHeading/SubHeading'
 import Layout from './Layout'
 
-export { Navbar, Footer, ProjectCard, SubHeading, Layout }
+export { Navbar, Footer, ProjectCard, SubHeading, Layout, SkillsCard }
