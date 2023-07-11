da8ff1f0c4b53118fb5c3add01ca921fb0a565cb
Ian Cenry <iancenry@gmail.com>
2023-07-11 23:14:19 +0300

    create skills component

---
+++ src/Components/SkillsCard/SkillsCard.jsx
@@ -0,0 +1,13 @@
+import './SkillsCard.scss'
+import PropTypes from 'prop-types'
+
+const SkillsCard = ({ title, content }) => {
+  return <div>SkillsCard</div>
+}
+
+SkillsCard.propTypes = {
+  title: PropTypes.string,
+  content: PropTypes.array,
+}
+
+export default SkillsCard
