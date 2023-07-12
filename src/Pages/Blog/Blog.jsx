9383ea40dc26dfad92379d8196a429561ecbd774
Ian Cenry <iancenry@gmail.com>
2023-07-12 20:38:25 +0300

    add and style blog page

---
+++ src/Pages/Blog/Blog.jsx
@@ -0,0 +1,12 @@
+import './Blog.scss'
+const Blog = () => {
+  return (
+    <div className="blog">
+      <div className="coming-soon">
+        <img src="/src/assets/images/coming-soon.png" alt="coming soon" />
+      </div>
+    </div>
+  )
+}
+
+export default Blog
