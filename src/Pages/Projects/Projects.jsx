17f8622cf6d3dfc8ad1df53d11d54c36c5fcfef0
Ian Cenry <iancenry@gmail.com>
2023-07-12 19:38:00 +0300

    complete Projects page

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,5 +1,40 @@
+import { useLocation } from 'react-router-dom'
+import './Projects.scss'
+import { SubHeading, ProjectCard, PageHeading } from '../../Components'
+import { useEffect, useState } from 'react'
+
 const Projects = () => {
-  return <div>Projects</div>
+  const location = useLocation()
+
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    fetch('/src/data/db.json')
+      .then((res) => res.json())
+      .then((data) => setData(data))
+  }, [])
+
+  const projects = data?.portfolio.map((portfolio) => (
+    <ProjectCard
+      key={portfolio.id}
+      cover={portfolio.cover}
+      title={portfolio.title}
+      description={portfolio.description}
+      languages={portfolio.languages}
+      link={portfolio.link}
+    />
+  ))
+
+  return (
+    <>
+      <section className="section projects">
+        <PageHeading path={location.pathname} />
+        <p className="p__firacode">List of my Projects</p>
+        <SubHeading subheading={'projects'} />
+        <div className="project-cards">{projects}</div>
+      </section>
+    </>
+  )
 }
 
 export default Projects
