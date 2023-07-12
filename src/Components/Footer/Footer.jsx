53f90cd858122007e7b9b6b389892a762f1cfcc2
Ian Cenry <iancenry@gmail.com>
2023-07-12 18:20:51 +0300

    complete footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,42 @@
+import { Link } from 'react-router-dom'
+import './Footer.scss'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
+
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer className="footer">
+      <div className="footer__info flex flex-jc-sb flex-ai-c">
+        <div className="footer__text">
+          <div className="top-text flex flex-ai-c">
+            <img src="/src/assets/images/logo.png" alt="" />
+            <p
+              className="p__firacode"
+              style={{ fontSize: '0.875rem', marginLeft: '1rem' }}
+            >
+              iancenry@gmail.com
+            </p>
+          </div>
+
+          <p>Full-Stack Developer and Software Engineer</p>
+        </div>
+        <div className="footer__media">
+          <h3 className="h3__firacode">Media</h3>
+          <Link to="https://github.com/iancenry" target="_blank">
+            <FaGithub color="#abb2bf" fontSize={20} />
+          </Link>
+          <Link to="." target="_blank">
+            <FaLinkedin color="#abb2bf" fontSize={20} />
+          </Link>
+        </div>
+      </div>
+      <p
+        className="p__firacode"
+        style={{ fontSize: '0.75rem', textAlign: 'center' }}
+      >
+        &copy; Copyright {new Date().getFullYear()}. Made by Ian Cenry
+      </p>
+    </footer>
+  )
 }
 
 export default Footer
