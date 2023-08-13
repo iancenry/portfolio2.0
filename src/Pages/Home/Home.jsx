4954a447b538f548852af8f9b43aa804a9793006
Ian Cenry <iancenry@gmail.com>
2023-08-13 23:11:27 +0300

    chore: edit asset paths

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -15,12 +15,13 @@ import python from '../../assets/images/stack/python.png'
 import node from '../../assets/images/stack/node-js.png'
 import sass from '../../assets/images/stack/sass.png'
 import skillsPattern from '../../assets/images/skills-pattern.png'
+import dbData from '../../data/db.json'
 
 const Home = () => {
   const [data, setData] = useState()
 
   useEffect(() => {
-    fetch('../../data/db.json')
+    fetch(dbData)
       .then((res) => res.json())
       .then((data) => setData(data))
   }, [])
